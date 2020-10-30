46e7f99907dc103fb9ebc9996f953f1acb700af3
kanako-110 <kanako.110.ito@gmail.com>
2020-10-30 23:11:16 +0900

    Homeの作成 #24

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ import Index from "./components/pages/Index";
 // import SignUp from "./components/pages/SignUp";
 
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Index} />
-        {/* <Route exact path="/home" component={Home} /> */}
-        {/* <Route exact path="/about" component={About} /> */}
-        {/* <Route exact path="/signin" component={SignIn} /> */}
-        {/* <Route exact path="/signup" component={SignUp} /> */}
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path="/" component={Index} />
+				{/* <Route exact path="/home" component={Home} /> */}
+				{/* <Route exact path="/about" component={About} /> */}
+				{/* <Route exact path="/signin" component={SignIn} /> */}
+				{/* <Route exact path="/signup" component={SignUp} /> */}
+			</Switch>
+		</Router>
+	);
 }
