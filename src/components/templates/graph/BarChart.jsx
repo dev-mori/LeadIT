eaa8e1288dfb44a8812c67bd6d36020ec4bfeb5f
bloodydark <justiceremain48694802@gmail.com>
2020-11-14 21:46:20 +0900

    Header/footerの実装 #38

--- src/components/templates/graph/BarChart.jsx
+++ src/components/templates/graph/BarChart.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 
-const Dankmemes = () => {
+const BarChart = () => {
   const [chartData, setChartData] = useState({});
 
   const chart = () => {
@@ -42,4 +42,4 @@ const Dankmemes = () => {
   );
 };
 
-export default Dankmemes;
+export default BarChart;
