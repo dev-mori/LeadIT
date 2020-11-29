124870b7c91d4e55f4041d843e0c3b3b1c7bbf58
kanako-110 <kanako.110.ito@gmail.com>
2020-11-29 21:23:53 +0900

    一旦pullのためにcommit

--- src/components/templates/Header/Achievement.jsx
+++ src/components/templates/Header/Achievement.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { useSelector } from "react-redux";
+import { AuthContext } from "../../../firebase/AuthService";
 
 export default function Achievement() {
   const dots = useSelector((state) => state.dots)
+  const user = useContext(AuthContext);
   console.log(dots)
+  console.log(user)
   return (<div>☆</div>)
 }
