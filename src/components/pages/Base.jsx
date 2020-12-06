e9c6175381f57ae89b6873ab92849145462bff2b
yuusaku125521 <one25Yellow@icloud.com>
2020-12-06 20:47:44 +0900

    FormコンポーネントBaseを作成 #83

--- src/components/pages/Base.jsx
+++ src/components/pages/Base.jsx
@@ -8,6 +8,7 @@ import Dots from "../templates/Dots";
 import firebase from "../../firebase/firebase";
 import { red } from "@material-ui/core/colors";
 // import UserIcon from "../templates/icons/user/user";
+import { Bodyleft } from "../templates/Header/HeaderElements";
 
 export default function Base() {
 	const logout = () => {
@@ -15,17 +16,20 @@ export default function Base() {
 	};
 
 	return (
-		<React.Fragment>
-			<Header />
+		<React.Fragment>	
+		<Header />	
+			<BarChart />
+			<Bodyleft>
+			<MiniForm />
+			<MiniDots />
+			</Bodyleft>
+		
+			{/* <UserIcon /> */}
+			<Footer />
 			<div>Index</div>
 			<button style={{ width: "100px", height: "30px" }} onClick={logout}>
 				ログアウト
 			</button>
-			<MiniForm />
-			<MiniDots />
-			<BarChart />
-			{/* <UserIcon /> */}
-			<Footer />
 		</React.Fragment>
 	);
 }
